798a7f35ebf951c7868228bbc1e80c4cd2ca930f
giulimon <giulimon>
2023-09-13 19:38:08 +0200

    newversion

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import timm
 import litdata
 import time
-
+device = "cuda" if torch.cuda.is_available() else "cpu"
 # Define a function to parse command-line arguments
 def parse_args():
     parser = argparse.ArgumentParser(description="Fine tuning ImageWoof Dataset")
